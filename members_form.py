89641f00c474c8ce463e58d32ae9ecfffede90e1
Temiyaks <temitopeyakub28@gmail.com>
2025-08-06 10:19:37 +0100

    add memebrsform

--- members_form.py
+++ members_form.py
@@ -11,7 +11,7 @@ from rapidfuzz import fuzz  # 👈 Fuzzy matching for names
 
 
 # === CONFIGURE FORM ACCESS WINDOW ===
-start_date = datetime(2025, 8, 1)  # 🗓️ Set this to form start date
+start_date = datetime(2025, 8, 6)  # 🗓️ Set this to form start date
 access_days = 5                  # ⏳ Number of days form stays open
 end_date = start_date + pd.Timedelta(days=access_days)
 today = datetime.now()
